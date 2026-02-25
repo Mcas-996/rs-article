df305463454df9ca697028a12f133004847d4fe1
Rezhe <rezhewang@gmail.com>
2026-02-25 18:56:33 +1100

    aCI

--- src/web.rs
+++ src/web.rs
@@ -1,12 +1,8 @@
+use console_error_panic_hook;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(start)]
 pub fn wasm_main() {
-    console_error_panic_hook_set();
-    web_sys::console::log_1(&"rs-grammar loaded in browser".into());
-}
-
-fn console_error_panic_hook_set() {
-    #[cfg(feature = "console_error_panic_hook")]
     console_error_panic_hook::set_once();
+    web_sys::console::log_1(&"rs-grammar loaded in browser".into());
 }
